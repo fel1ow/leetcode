e2f5040a2b47dc9c5dada16e4ea806d17553a9d6
Dmitriy Fellow <fellow0@gmail.com>
2022-06-04 23:54:20 +0300

    1050 done

---
+++ 1050.sql
@@ -0,0 +1,23 @@
+create table ActorDirector
+(
+    actor_id int,
+    director_id int,
+    timestamp int primary key
+)
+;
+insert into ActorDirector
+(actor_id, director_id, timestamp)
+values
+(1, 1, 0),
+(1, 1, 1),
+(1, 1, 2),
+(1, 2, 3),
+(1, 2, 4),
+(2, 1, 5),
+(2, 1, 6)
+;
+select actor_id, director_id
+from ActorDirector
+group by actor_id, director_id
+having COUNT(*) >= 3
+;
